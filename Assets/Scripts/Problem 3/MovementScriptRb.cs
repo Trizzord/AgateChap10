2dc4e1e2e08681610cf5d9f43010ef68b85211a1
Trizzord <trizzordfullfree@gmail.com>
2021-10-10 17:28:29 +0700

    Problem 8

---
+++ Assets/Scripts/Problem 3/MovementScriptRb.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScriptRb : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        //this.transform.Translate(Vector2.one * Time.timeScale * 0.1f);
+        rigidBody2D.AddForce(new Vector2(3, 1));
+    }
+}
