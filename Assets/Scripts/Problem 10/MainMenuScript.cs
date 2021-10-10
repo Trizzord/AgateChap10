5d7a26e766d164912b170e0f5076213eb324928c
Trizzord <trizzordfullfree@gmail.com>
2021-10-10 17:32:47 +0700

    Problem 10 - Complete Version

---
+++ Assets/Scripts/Problem 10/MainMenuScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public GameObject MainMenu, PlayMenu;
+    void Start()
+    {
+        MainMenu.SetActive(true);
+        PlayMenu.SetActive(false);
+    }
+    public void PlayButton()
+    {
+        MainMenu.SetActive(false);
+        PlayMenu.SetActive(true);
+    }
+    public void Level1Button()
+    {
+        SceneManager.LoadScene("Problem 1");
+    }
+    public void Level2Button()
+    {
+        SceneManager.LoadScene("Problem 2");
+    }
+    public void Level3Button()
+    {
+        SceneManager.LoadScene("Problem 3");
+    }
+    public void Level4Button()
+    {
+        SceneManager.LoadScene("Problem 4");
+    }
+    public void Level5Button()
+    {
+        SceneManager.LoadScene("Problem 5");
+    }
+    public void Level6Button()
+    {
+        SceneManager.LoadScene("Problem 6");
+    }
+    public void Level7Button()
+    {
+        SceneManager.LoadScene("Problem 7");
+    }
+    public void Level8Button()
+    {
+        SceneManager.LoadScene("Problem 8");
+    }
+    public void Level9Button()
+    {
+        SceneManager.LoadScene("Problem 9");
+    }
+    public void BackButton()
+    {
+        MainMenu.SetActive(true);
+        PlayMenu.SetActive(false);
+    }
+    public void ExitButton()
+    {
+        Debug.Log("Exit from Game");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Problem 4/MovementUseKey.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementUseKey : MonoBehaviour
+{
+    public float speed = 1f;
+    Vector2 movement;
+    private Rigidbody2D rigidBody2D;
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Momement(h, v);
+    }
+    void Momement(float h, float v)
+    {
+        movement.Set(h, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        Vector2 position = transform.position;
+        rigidBody2D.MovePosition(position + movement);
+    }
+}
